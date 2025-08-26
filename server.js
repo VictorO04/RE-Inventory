dbf6606af6142f466c0ddd0b41083cd398f1fc72
Victor <Victorfoliveira0407@gmail.com>
2025-08-25 21:53:48 -0300

    Implementação get by id e get by name

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dados from "./src/data/dados.js";
+const {armasRE4} = dados;
+
+const app = express();
+const serverPort = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Site de Resident Evil");
+});
+
+app.get("/Resident-Evil-4/armas", (req, res) => {
+    res.status(200).json(armasRE4);
+});
+
+app.get("/Resident-Evil-4/armas/:id", (req, res) => {
+    let id = req.params.id;
+    id = parseInt(id);
+    const idEncontrado = armasRE4.find(i => i.id === id);
+
+    if (idEncontrado) {
+        res.status(200).json(idEncontrado);
+    } else {
+        res.status(404).json({
+            "erro": `Arma com o id ${id} não encontrada!`
+        });
+    }
+});
+
+app.get("/Resident-Evil-4/armas/nome/:nome", (req, res) => {
+    let nome = req.params.nome.toLowerCase();
+    const armaEncontrada = armasRE4.filter(a => a.nome.toLowerCase().includes(nome));
+
+    if (armaEncontrada.length > 0) {
+        res.status(200).json(armaEncontrada);
+    } else {
+        res.status(404).json({
+            "erro": `Arma com o nome ${nome} não encontrada!`
+        });
+    }
+});
+
+app.listen(serverPort, () => {
+    console.log(`O servidor está aberto em: http://localhost:${serverPort}`);
+})
---
+++ src/data/dados.js
@@ -0,0 +1,73 @@
+const armasRE4 = [
+    {
+        "id": 1,
+        "nome": "Handgun",
+        "tipo": "Pistola",
+        "danoInicial": 1,
+        "danoFinal": 2,
+        "velocidadeDisparoInicial": 0.47,
+        "velocidadeDisparoFinal": 0.33,
+        "velocidadeRecargaInicial": 1.73,
+        "velocidadeRecargaFinal": 0.83,
+        "CapacidadeInicial": 10,
+        "CapacidadeFinal": 25,
+        "Exclusivo": "Aumenta a chance de acerto crítico em 5 vezes"
+    },
+    {
+        "id": 2,
+        "nome": "Punisher",
+        "tipo": "Pistola",
+        "danoInicial": 0.9,
+        "danoFinal": 1.9,
+        "velocidadeDisparoInicial": 0.47,
+        "velocidadeDisparoFinal": 0.33,
+        "velocidadeRecargaInicial": 1.73,
+        "velocidadeRecargaFinal": 0.83,
+        "CapacidadeInicial": 10,
+        "CapacidadeFinal": 28,
+        "Exclusivo": "Poder de perfuração aumentado, podendo atingir até 5 inimigos enfileirados"
+    },
+    {
+        "id": 3,
+        "nome": "Red9",
+        "tipo": "Pistola",
+        "danoInicial": 1.6,
+        "danoFinal": 3.7,
+        "velocidadeDisparoInicial": 0.53,
+        "velocidadeDisparoFinal": 0.40,
+        "velocidadeRecargaInicial": 2.37,
+        "velocidadeRecargaFinal": 1.67,
+        "CapacidadeInicial": 8,
+        "CapacidadeFinal": 22,
+        "Exclusivo": "Dano aumentado até 6,5"
+    },
+    {
+        "id": 4,
+        "nome": "Blacktail",
+        "tipo": "Pistola",
+        "danoInicial": 1.6,
+        "danoFinal": 3.2,
+        "velocidadeDisparoInicial": 0.47,
+        "velocidadeDisparoFinal": 0.27,
+        "velocidadeRecargaInicial": 1.70,
+        "velocidadeRecargaFinal": 0.83,
+        "CapacidadeInicial": 15,
+        "CapacidadeFinal": 35,
+        "Exclusivo": "Dano aumentado até 4,5"
+    },
+    {
+        "id": 5,
+        "nome": "Matilda",
+        "tipo": "Pistola",
+        "danoInicial": 1.5,
+        "danoFinal": 2.5,
+        "velocidadeDisparo": 0.47,
+        "velocidadeRecargaInicial": 1.73,
+        "velocidadeRecargaFinal": 0.87,
+        "CapacidadeInicial": 15,
+        "CapacidadeFinal": 30,
+        "Exclusivo": "Capacidade aumentada até 100 balas"
+    }
+];
+
+export default {armasRE4}
